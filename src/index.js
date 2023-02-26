8ab4c4484446eab94d7fdf3332650af5262fa562
Thiago <thiagofkm@gmail.com>
2023-02-26 17:05:51 -0300

    Página de serviços

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-import "./scss/main.scss";
+import "./scss/main.scss"
+import saladImg from "./assets/img/salad.png"
+import chefHatImg from "./assets/img/chef-hat.png"
+import truckImg from "./assets/img/truck.png"
 
+document.querySelector("[data-src='salad.png']").src = saladImg
+document.querySelector("[data-src='chef-hat.png']").src = chefHatImg
+document.querySelector("[data-src='truck.png']").src = truckImg
