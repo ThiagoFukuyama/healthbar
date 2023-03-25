34dbceee79d5bedb46409b7111ab45bdd7198b7b
Thiago Fukuyama Marcilli <thiagofkm@gmail.com>
2023-03-25 15:23:25 -0300

    Swiper ignorado no pelo PurgeCSS

--- webpack.prod.js
+++ webpack.prod.js
@@ -28,6 +28,9 @@ module.exports = merge(common, {
         }),
         new PurgeCSSPlugin({
             paths: glob.sync("./**/*.html"),
+            safelist: {
+                greedy: [/swiper/],
+            }
         }),
     ],
 })
