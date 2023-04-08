964e09119582f76e207b319e2a4af25b81df566f
Thiago Fukuyama Marcilli <thiagofkm@gmail.com>
2023-04-08 13:50:00 -0300

    Logo da header adicionado quando Nav é expandida

--- src/js/toggleNavigation.js
+++ src/js/toggleNavigation.js
@@ -1,10 +1,14 @@
 
 const navToggle = document.querySelector("button[aria-controls='primary-navigation'")
+const headerLogo = document.querySelector(".primary-header .logo")
 
 navToggle.addEventListener("click", toggleNavigation)
 
 function toggleNavigation() {
     let isExpanded = navToggle.getAttribute("aria-expanded") === "false" ? "true" : "false"
     navToggle.setAttribute("aria-expanded", isExpanded) 
+
+    headerLogo.classList.toggle("logo--navigation-expanded")
     document.body.classList.toggle("scroll-locked")
 }
+
