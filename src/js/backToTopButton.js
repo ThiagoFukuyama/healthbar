78be2363044640a16a0c1aad391a1e4dda561daf
Thiago Fukuyama Marcilli <thiagofkm@gmail.com>
2023-04-28 22:30:29 -0300

    Redefinido quando o botão scroll to top aparece

--- src/js/backToTopButton.js
+++ src/js/backToTopButton.js
@@ -1,20 +1,19 @@
-
-const backToTopButton = document.getElementById("back-to-top-button")
+const backToTopButton = document.getElementById("back-to-top-button");
 
 backToTopButton.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-    })
-})
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
 
 window.addEventListener("scroll", toggleBackToTopButton);
 window.addEventListener("load", toggleBackToTopButton);
 
 function toggleBackToTopButton() {
-    if (window.scrollY > (window.innerHeight * 1.1)) {
-        backToTopButton.classList.add("active");
-    } else {
-        backToTopButton.classList.remove("active");
-    }
+  if (window.scrollY > 400) {
+    backToTopButton.classList.add("active");
+  } else {
+    backToTopButton.classList.remove("active");
+  }
 }
