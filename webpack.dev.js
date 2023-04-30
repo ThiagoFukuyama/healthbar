f1734541b19a8fa81c024436ad85fbee54b90ac8
Thiago Fukuyama Marcilli <thiagofkm@gmail.com>
2023-04-30 16:16:22 -0300

    devServer abre navegador padrão automaticamente

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,30 +1,26 @@
-const path = require("path")
-const { merge } = require("webpack-merge")
-const common = require("./webpack.common.js")
+const path = require("path");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    mode: "development",
-    devtool: "inline-source-map",
-    module: {
-        rules: [
-            {
-                test: /\.(sass|css|scss)$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "postcss-loader",
-                    "sass-loader",
-                ]
-            },
-        ],
+  mode: "development",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(sass|css|scss)$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
     },
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, "dist"),
-        },
-        port: 3000,
-        hot: true,
-        compress: true,
-        historyApiFallback: true,
-    },
-})
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+});
